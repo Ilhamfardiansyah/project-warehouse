ab446ffded2010b3b01b14cf0a259e6b1feb4b3f
ilham.fardiansyah <93816433+Ilhamfardiansyah@users.noreply.github.com>
2022-02-21 20:46:29 +0700

    Edit Post

--- app/Models/post.php
+++ app/Models/post.php
@@ -2,35 +2,10 @@
 
 namespace App\Models;
 
-class Post 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts =  [
-    [
-        "title" => "Judul Post Pertama",
-        "slug" => "judul-post-pertama",
-        "author" => "Ilham Fardiansyah",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing 
-        elit. Nobis sint quaerat quod, laborum pariatur repellat ipsam adipisci officia reiciendis doloribus, beatae vero tempore eum quisquam in hic et magni deleniti!
-"
-    ],
-    [ 
-        "title" => "Judu Post Kedua",
-        "slug" => "judul-post-kedua",
-        "author" => "Jaya Wijaya",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing 
-        elit. Nobis sint quaerat quod, laborum pariatur repellat ipsam adipisci officia reiciendis doloribus, beatae vero tempore eum quisquam in hic et magni deleniti!
-"
-    ]
-];
-
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts -> firstWhere('slug', $slug);
-    }
+    use HasFactory;
 }
