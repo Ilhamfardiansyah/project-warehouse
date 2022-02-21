b166073398386de2e7862aaf5cb5ec4074d85bcd
ilham.fardiansyah <93816433+Ilhamfardiansyah@users.noreply.github.com>
2022-02-21 21:06:15 +0700

    Tambah Table Post

--- database/migrations/2022_02_21_134429_create_posts_table.php
+++ database/migrations/2022_02_21_134429_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('excerpt');
+            $table->text('body');
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
