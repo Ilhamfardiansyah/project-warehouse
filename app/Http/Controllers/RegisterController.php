472afbef568006751c00dfcf9058a8d9e00e87bf
ilham.fardiansyah <93816433+Ilhamfardiansyah@users.noreply.github.com>
2022-02-22 23:20:55 +0700

    Tambah Login dan Dashboard

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -13,4 +14,22 @@ class RegisterController extends Controller
             'active' => 'register'
         ]);
     } 
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3','max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:10'
+        ]);
+
+        $validateData['password'] = bcrypt($validateData['password']);
+    // $validateData['password'] = Hash::make($validateData['password']);
+    User::create($validateData);
+    
+    // $request->session()->flash('succes', 'Anda Berhasil Mebuat Akun');
+    
+    return redirect('/login')->with('succes', 'Anda Berhasil Mebuat Akun');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Models\Post;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('home', [
@@ -27,5 +28,10 @@ Route::get('/blog', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 // Login
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
 
 Route::get('/register', [RegisterController::class, 'index']);
+
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
